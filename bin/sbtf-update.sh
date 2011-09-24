f3cb71a8f13ca9925e1757ff1f2dc0b7fb8c7c2f
Nigel McNie <nigel@mcnie.name>
2011-09-24 15:13:16 +1200

    Add sbtf-update.sh script. Re-runs puppet. sbtf user has permission to run this via sudo.

---
+++ bin/sbtf-update.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Ensures GYGO environments are up to date
+#
+# Usage: ./bin/gygo-update.sh
+#
+
+SCRIPT=$(readlink -f $0)
+SCRIPTDIR=$(dirname $SCRIPT)
+CODEDIR=$(cd "$SCRIPTDIR/.." ; pwd)
+
+bail () {
+    echo $1
+    exit 1
+}
+
+if [ "$(id -u)" != "0" ]; then
+    bail "This script should be run as 'root'"
+fi
+
+if [ ! -d "$CODEDIR/.git" ]; then
+    bail "Could not find .git for your repository that is supposed to be at $CODEDIR"
+fi
+
+puppet $CODEDIR/puppet/site.pp
+RET=$?
+
+if [ $RET -gt 0 ]; then
+    echo "Configuration of your environment by puppet failed! Please correct any"
+    echo "problems it encountered and run this script again."
+    exit 1
+fi
+
+echo "OK"
